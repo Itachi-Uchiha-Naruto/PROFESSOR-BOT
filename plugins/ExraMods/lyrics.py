bd94e505cbe7ddb4c7e08ed794f49cc70339834b
Itachi-Uchiha-Naruto <135048674+Itachi-Uchiha-Naruto@users.noreply.github.com>
2023-05-30 22:22:20 +0530

    Update lyrics.py

--- plugins/ExraMods/lyrics.py
+++ plugins/ExraMods/lyrics.py
@@ -20,9 +20,9 @@ async def sng(bot, message):
           await mee.delete()
           try:
             await mee.delete()
-            await bot.send_message(chat_id, text = rpl, reply_to_message_id = message.id, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs ", url = f"t.me/Team_idiots")]]))
+            await bot.send_message(chat_id, text = rpl, reply_to_message_id = message.id, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Team Idiots ", url = f"t.me/Team_idiots")]]))
           except Exception as e:                            
-             await message.reply_text(f"I Can't Find A Song With `{song}`", quote = True, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url = f"t.me/Team_idiots")]]))
+             await message.reply_text(f"I Can't Find A Song With `{song}`", quote = True, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Team Idiotss", url = f"t.me/Team_idiots")]]))
 
 
 def search(song):
